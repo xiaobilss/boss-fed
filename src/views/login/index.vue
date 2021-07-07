333067c45feb92bdcd2c0e96f44df773254d17ee
雨泣 <panjunyu10@163.com>
2021-07-07 19:53:58 +0800

    ax

---
+++ src/views/login/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>LoginIndex</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+export default Vue.extend({
+    name:"LoginIndex"
+})
+</script>
+
+<style lang="scss" scoped></style>
