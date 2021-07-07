4ee3908ef402a9481114b94609d8d3b5c5f8f871
雨泣 <panjunyu10@163.com>
2021-07-07 18:06:34 +0800

    axios

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,27 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-06 11:02:34
+ * @LastEditTime: 2021-07-07 11:00:39
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \boss-fed\.eslintrc.js
+ */
 module.exports = {
   root: true,
   env: {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
+    "plugin:vue/essential",
+    "@vue/standard",
+    "@vue/typescript/recommended"
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    // semi: "off"
   }
-}
+};
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,64 @@
 <template>
   <div id="app">
     <h1>雨泣</h1>
+    <!-- <button @click="text" class="btn">{{ a }}</button> -->
+    <el-button>默认按钮</el-button>
     <router-view />
   </div>
 </template>
 
-<style lang="sass" scoped>
+<script lang="ts">
+import Vue from 'vue'
+// import Component from 'vue-class-component'
+import request from '@/uitls/request'
+
+request({
+  method: 'GET',
+  url: '/boss'
+}).then(res => {
+  console.log(res)
+})
+
+export default Vue.extend({
+  data () {
+    return {
+      a: 12
+    }
+  },
+  methods: {
+    text () {
+      console.log(this.a)
+    }
+  }
+})
+</script>
+<!--
+ <script>
+// export default {
+//   data () {
+//     return {
+//       a: '111',
+//       b: 1,
+//       c: {
+//         name: 'zhangsan',
+//         age: 12
+//       }
+//     }
+//   },
+//   methods: {
+//     text () {
+//       console.log(this.b.age)
+//     }
+//   }
+// }
+// </!-->
+-->
+<style lang="scss" scoped>
+
+.btn {
+  width: 1  00px;
+  height: 30px;
+  background-color: $danger-color;
+  color: $warning-color;
+}
 </style>
